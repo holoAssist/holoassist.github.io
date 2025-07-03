6e928669b9a9b079ec531357a4d70441632f4b15
mahdirad <mahdirad@microsoft.com>
2025-07-03 17:16:23 +0200

    fix holoassist.gif

--- js/custom.js
+++ js/custom.js
@@ -3,24 +3,24 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  */
 
-(function ($) {
-	$(document).ready(function () {
-
+(function($){
+	$(document).ready(function(){
+	
 		// Add alt attribute to backstretch image for accessibility
 		$(".banner-image").backstretch('images/holoassist.gif');
 		// Wait for the image to be added, then set alt attribute
-		setTimeout(function () {
-			var $img = $(".banner-image .backstretch img");
-			if ($img.length) {
-				$img.attr("alt", "HoloAssist demonstration animation");
-			}
+		setTimeout(function() {
+		  var $img = $(".banner-image .backstretch img");
+		  if ($img.length) {
+		    $img.attr("alt", "HoloAssist demonstration animation");
+		  }
 		}, 100);
-
+		
 		// Fixed header
 		//-----------------------------------------------
-		$(window).scroll(function () {
-			if (($(".header.fixed").length > 0)) {
-				if (($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+		$(window).scroll(function() {
+			if (($(".header.fixed").length > 0)) { 
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
 					$("body").addClass("fixed-header-on");
 				} else {
 					$("body").removeClass("fixed-header-on");
@@ -28,25 +28,25 @@
 			};
 		});
 
-		$(window).load(function () {
-			if (($(".header.fixed").length > 0)) {
-				if (($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+		$(window).load(function() {
+			if (($(".header.fixed").length > 0)) { 
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
 					$("body").addClass("fixed-header-on");
 				} else {
 					$("body").removeClass("fixed-header-on");
 				}
 			};
 		});
-
-		$('#quote-carousel').carousel({
-			pause: true,
-			interval: 4000,
-		});
+		
+	   $('#quote-carousel').carousel({
+		 pause: true,
+		 interval: 4000,
+	   });
 		//Scroll Spy
 		//-----------------------------------------------
-		if ($(".scrollspy").length > 0) {
+		if($(".scrollspy").length>0) {
 			$("body").addClass("scroll-spy");
-			$('body').scrollspy({
+			$('body').scrollspy({ 
 				target: '.scrollspy',
 				offset: 152
 			});
@@ -54,14 +54,14 @@
 
 		//Smooth Scroll
 		//-----------------------------------------------
-		if ($(".smooth-scroll").length > 0) {
-			$('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function () {
-				if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		if ($(".smooth-scroll").length>0) {
+			$('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function() {
+				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 					var target = $(this.hash);
-					target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 					if (target.length) {
 						$('html,body').animate({
-							scrollTop: target.offset().top - 151
+							scrollTop: target.offset().top-151
 						}, 1000);
 						return false;
 					}
@@ -71,16 +71,16 @@
 
 		// Animations
 		//-----------------------------------------------
-		if (($("[data-animation-effect]").length > 0) && !Modernizr.touch) {
-			$("[data-animation-effect]").each(function () {
+		if (($("[data-animation-effect]").length>0) && !Modernizr.touch) {
+			$("[data-animation-effect]").each(function() {
 				var $this = $(this),
-					animationEffect = $this.attr("data-animation-effect");
-				if (Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
-					$this.appear(function () {
-						setTimeout(function () {
+				animationEffect = $this.attr("data-animation-effect");
+				if(Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
+					$this.appear(function() {
+						setTimeout(function() {
 							$this.addClass('animated object-visible ' + animationEffect);
 						}, 400);
-					}, { accX: 0, accY: -130 });
+					}, {accX: 0, accY: -130});
 				} else {
 					$this.addClass('object-visible');
 				}
@@ -89,8 +89,8 @@
 
 		// Isotope filters
 		//-----------------------------------------------
-		if ($('.isotope-container').length > 0) {
-			$(window).load(function () {
+		if ($('.isotope-container').length>0) {
+			$(window).load(function() {
 				$('.isotope-container').fadeIn();
 				var $container = $('.isotope-container').isotope({
 					itemSelector: '.isotope-item',
@@ -99,7 +99,7 @@
 					filter: "*"
 				});
 				// filter items on button click
-				$('.filters').on('click', 'ul.nav li a', function () {
+				$('.filters').on( 'click', 'ul.nav li a', function() {
 					var filterValue = $(this).attr('data-filter');
 					$(".filters").find("li.active").removeClass("active");
 					$(this).parent().addClass("active");
@@ -111,9 +111,9 @@
 
 		//Modal
 		//-----------------------------------------------
-		if ($(".modal").length > 0) {
-			$(".modal").each(function () {
-				$(".modal").prependTo("body");
+		if($(".modal").length>0) {
+			$(".modal").each(function() {
+				$(".modal").prependTo( "body" );
 			});
 		}
 
